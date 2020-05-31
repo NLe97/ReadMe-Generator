cfab25611ed2b01c6ad7a198aa640de10c4d0c0e
Ben Le <lebenjamin77@yahoo.com>
2020-05-31 13:29:29 -0600

    adding function to genMarkDown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,11 @@
 function generateMarkdown(data) {
   return `
-# ${data.title}
-
+# **${data.title.trim()}**\n
+![Github](https://img.shields.io/github/last-commit/${data.username.trim()}/${data.title.trim()})\n
+## **Description**\n ${data.description.trim()}\n
+## **Table of Contents**\n
+* Installation\n
+* 
 `;
 }
 
