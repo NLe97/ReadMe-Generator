6d8c031c63cf12824371f292380bcfc749db2f93
Ben Le <lebenjamin77@yahoo.com>
2020-06-01 15:06:06 -0600

    add genMarkdown function to index and utils genMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,8 +4,29 @@ function generateMarkdown(data) {
 ![Github](https://img.shields.io/github/last-commit/${data.username.trim()}/${data.title.trim()})\n
 ## **Description**\n ${data.description.trim()}\n
 ## **Table of Contents**\n
-* Installation\n
-* 
+* Installation
+* Usage
+* License
+* Contributing
+* Tests
+* Questions
+
+## ** Installation**\n
+In order to install the dependencies, run the command:\n
+\`${data.install.trim()}\`\n
+## **Usage**\n ${data.usage.trim()}\n
+## **License**\n
+This project is licensed by ${data.license.trim()} license.\n
+## **Contributing**\n
+${data.contribution.trim()}\n
+## **Tests**\n
+To run tests, run the command:\n
+\`${data.test.trim()}\`\n
+## **Questions**\n
+<img src="https://avatars.githubusercontent.com/${data.username.trim()}">\n
+For question on this repo, report issue and contact ${data.email.trim()}
+
+
 `;
 }
 
